2e685a3eff8e2e8958aeffbd27aff8e6327100d8
joshua.koch <joshua.koch@comnetgmbh.com>
2023-06-07 13:42:58 +0200

    Add prototype of posts

--- src/components/Sites/ForYou.vue
+++ src/components/Sites/ForYou.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import {posts} from "../../data/pictures.ts";
+import PostCard from "./PostCard.vue";
+import {ref} from "vue";
+
+
 
 </script>
 
 <template>
-<h1>2</h1>
+    <h1>For You Page</h1>
+    <PostCard :post="item" v-for="item in posts" />
 </template>
 
 <style scoped>
--- src/components/Sites/Homepage.vue
+++ src/components/Sites/Homepage.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import {posts} from "../../data/pictures.ts";
+import PostCard from "./PostCard.vue";
+import {ref} from "vue";
+
+const last_10_posts = ref(posts.value.slice(-10))
+
+
 
 </script>
 
 <template>
-<h1>1</h1>
+    <h1>Homepage</h1>
+    <PostCard :post="item" v-for="item in last_10_posts" />
 </template>
 
 <style scoped>
